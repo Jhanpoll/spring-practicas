00733c05e95a831d1bd7074539591934ae01e4eb
Jesus Chero <cheroarge96@gmail.com>
2022-06-27 09:12:17 -0500

    USO DE ROWMAPPER CON CLASE ANONIMA

--- src/main/java/com/curso/jdbc/SpringCursoJdbcApplication.java
+++ src/main/java/com/curso/jdbc/SpringCursoJdbcApplication.java
@@ -1,5 +1,7 @@
 package com.curso.jdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -14,6 +16,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.curso.jdbc.mappers.EmployeeRowMapper;
 import com.curso.jdbc.models.entity.Employee;
@@ -31,16 +34,33 @@ public class SpringCursoJdbcApplication implements ApplicationRunner {
 		
 		//INICIO DE SEGUNDO BLOQUE
 		 
-		List<Employee> employees= template.query("select * from employee", new EmployeeRowMapper());
+		List<Employee> employees= template.query("select * from employee", new RowMapper<Employee>() {
+			// implementando la clase anonima
+			@Override
+			public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Employee employee = new Employee();
+				employee.setId(rs.getInt(1));
+				employee.setNombre(rs.getString("nombre"));
+				employee.setApellido(rs.getString("apellido"));
+				employee.setAge(rs.getInt("age"));
+				employee.setSalario(rs.getDouble("salario"));
+				
+				
+				return employee;
+			}
+			
+		});
 		
 		for (Employee employee : employees) {
 			log.info("id{} , nombre {}, apellido{}, edad{}, salario{}",
 					employee.getId(),
 					employee.getNombre(),
 					employee.getApellido(),
-					employee.getAge(),employee.
-					getSalario());
+					employee.getAge(),
+					employee.getSalario()
+					);
 		}
+		
 		
 		//FIN DEL BLOQUE 2
 		
