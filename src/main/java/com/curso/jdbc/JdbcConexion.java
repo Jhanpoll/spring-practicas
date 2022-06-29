aca79ace648a7600161f6c82b405e194453b3c89
Jesus Chero <cheroarge96@gmail.com>
2022-06-28 19:51:07 -0500

    uso de preparedStatement , executeQuery , next()

--- src/main/java/com/curso/jdbc/JdbcConexion.java
+++ src/main/java/com/curso/jdbc/JdbcConexion.java
@@ -1,8 +1,14 @@
 package com.curso.jdbc;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import org.h2.tools.RunScript;
 
 public class JdbcConexion {
 	
@@ -10,14 +16,14 @@ public class JdbcConexion {
 		
 		try {
 			// conectando con bd H2
-			//connection = DriverManager.getConnection("jdbc:h2:~/test");
+			Connection connection = DriverManager.getConnection("jdbc:h2:~/test");
 			
 			// CONECTANDOME CON LA BD MYSQL
 			System.out.println("connecting...");
 			
-			 Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_spring_jdbc","root","CHERO123");
+			 //Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_spring_jdbc","root","CHERO123");
 			System.out.println("conected");
-			/*
+			
 			System.out.println("executing script");
 			RunScript.execute(connection, new FileReader("src/main/resources/prueba.sql"));
 			System.out.println("insert new person");
@@ -29,12 +35,38 @@ public class JdbcConexion {
 			rowStatement.setString(3,"javalover");
 			int row = rowStatement.executeUpdate();
 			System.out.println("row impacted : "+ row);
+			
+			rowStatement.setString(1,"jhanpoll");
+			rowStatement.setString(2,"arge");
+			rowStatement.setString(3,"c#lover");
+			
+			row = rowStatement.executeUpdate();
+			
+			System.out.println("row impacted : "+ row);
+			
 			rowStatement.close();
+			
+			/*realizar una query : select * from tabla*/
+			PreparedStatement queryStatement = connection.prepareStatement("select * from person");
+			ResultSet executeQuery = queryStatement.executeQuery();
+			while(executeQuery.next()) {
+				System.out.printf("\nid[%d] nombre[%s] apellid[%s] nickname[%s]", executeQuery.getInt(1),executeQuery.getString(2),executeQuery.getString(3),executeQuery.getString(4));
+				
+			}
+			
+			/*eliminar registros*/
+			
+			PreparedStatement deleteStatement = connection.prepareStatement("DELETE FROM person");
+			int rowDelete = deleteStatement.executeUpdate();
+			System.out.println("");
+			System.out.println("row deleted : "+rowDelete);
+			deleteStatement.close();
+			
 			System.out.println("closing..");
 			connection.close();
 			System.out.println("closed");
-			*/
-		} catch (SQLException  e) {
+			
+		} catch (SQLException | FileNotFoundException  e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
